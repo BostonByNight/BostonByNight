77a8f5d8f6463a741c237fd503ab0b5817ffc5ac
MaxDac <massimiliano.dacunzo@hotmail.com>
2022-10-07 13:57:48 +0100

    Fixing automatic migrations

--- .gitpod/init.sh
+++ .gitpod/init.sh
@@ -6,6 +6,11 @@ psql --command "CREATE USER postgres WITH SUPERUSER PASSWORD 'mysecretpassword';
 # Installing Back End dependencies
 mix deps.get && mix deps.compile
 
+# Creating and migrating database
+mix ecto.create
+cd apps/vtm_auth && mix ecto.migrate && cd ../../
+cd apps/vtm && mix ecto.migrate && cd ../../
+
 # Installing Front End dependencies
 yarn --cwd apps/vtm_web/assets install
 
