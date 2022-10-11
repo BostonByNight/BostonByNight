c4b472d6d614566f74e945cbc174774964707553
MaxDac <massimiliano.dacunzo@hotmail.com>
2022-10-11 16:57:19 +0100

    Adding automatic seedig for gitpod (#16)

--- .gitpod/init.sh
+++ .gitpod/init.sh
@@ -11,6 +11,9 @@ mix ecto.create
 cd apps/vtm_auth && mix ecto.migrate && cd ../../
 cd apps/vtm && mix ecto.migrate && cd ../../
 
+mix run apps/vtm_auth/priv/repo/seeds.exs
+mix run apps/vtm/priv/repo/seeds.exs
+
 # Installing Front End dependencies
 yarn --cwd apps/vtm_web/assets install
 yarn --cwd apps/vtm_web/assets build
