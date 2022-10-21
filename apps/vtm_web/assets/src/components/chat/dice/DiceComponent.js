b53053a4c49b28b739a9db5ebe1b23633721bbd1
MaxDac <massimiliano.dacunzo@hotmail.com>
2022-10-21 16:35:35 +0100

    Refactored Dices

--- apps/vtm_web/assets/src/components/chat/dice/DiceComponent.js
+++ apps/vtm_web/assets/src/components/chat/dice/DiceComponent.js
@@ -1,4 +1,5 @@
 // @flow
+
 import React from 'react';
 import ReactMarkdown from "react-markdown";
 import Dice from './Dice.js';
@@ -10,46 +11,49 @@ type DiceComponentProps = {
 }
 
 const DiceComponent = ({result, components}: DiceComponentProps): GenericReactComponent => {
-    let difficulty = result.substring(result.indexOf('difficolt'), result.length - 1);
-    let parsedResultText = result.substring(0, result.indexOf('(')) + ' - ' + difficulty;
+    const difficulty = result.substring(result.indexOf('difficolt'), result.length - 1);
+    const parsedResultText = result.substring(0, result.indexOf('(')) + ' - ' + difficulty;
 
-    const createDices= () => {
-        let results = result.substring(result.indexOf('(') + 1, result.indexOf(', difficolt')).split(', ');
-        return results.map((el, index) => {
-            if(el) {
-                let src, alt;
-                if (el.charAt(0) === '*') {
-                    let parsedEl = Number(el.replace('*', ''));
-                    if (parsedEl === 1) {
-                        src="/dice/bestial-fail.webp";
-                        alt="fallimento bestiale";
-                    } else if (parsedEl < 6) {
-                        src="/dice/red-fail.webp";
-                        alt="fallimento";
-                    } else if (parsedEl === 10) {
-                        src="/dice/red-crit.webp";
-                        alt="critico bestiale";
-                    } else {
-                        src="/dice/red-success.webp";
-                        alt="successo";
-                    }
-                } else {
-                    el = Number(el);
-                    if (el < 6) {
-                        src="/dice/normal-fail-white.webp";
-                        alt="fallimento";
-                    } else if (el === 10) {
-                        src="/dice/normal-crit-white.webp";
-                        alt="successo critico";
-                    } else {
-                        src="/dice/normal-success-white.webp";
-                        alt="successo";
-                    }
-                }
-                return <Dice src ={src} alt={alt} key={index} />
-            }
-        })
+    const getBeastDiceComponent = (diceResult: number) => {
+        if (diceResult === 1) {
+            return ["/dice/bestial-fail.webp", "fallimento bestiale"]
+        } else if (diceResult < 6) {
+            return ["/dice/red-fail.webp", "fallimento"]
+        } else if (diceResult === 10) {
+            return ["/dice/red-crit.webp", "critico bestiale"]
+        } else {
+            return ["/dice/red-success.webp", "successo"]
+        }
     }
+
+    const getNormalDiceComponent = (diceResult: number) => {
+        if (diceResult < 6) {
+            return ["/dice/normal-fail-white.webp", "fallimento"]
+        } else if (diceResult === 10) {
+            return ["/dice/normal-crit-white.webp", "successo critico"]
+        } else {
+            return ["/dice/normal-success-white.webp", "successo"]
+        }
+    }
+
+    const getDiceComponent = (diceResult: string, index: number) => {
+        const [first] = diceResult
+        const rest = diceResult.slice(0)
+        const isBeastDice = first === "*"
+
+        const [src, alt] =
+            isBeastDice
+                ? getBeastDiceComponent(Number(rest.replace("*", "")))
+                : getNormalDiceComponent(Number(rest))
+
+        return <Dice src ={src} alt={alt} key={index} />
+    }
+
+    const createDices= () => result
+        .substring(result.indexOf('(') + 1, result.indexOf(', difficolt'))
+        .split(', ')
+        .filter(ex => ex != null)
+        .map(getDiceComponent)
 
     return (
         <>
